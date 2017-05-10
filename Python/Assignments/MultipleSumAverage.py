48283eaca9eb6dae77f97c79467eeb1daec89bc2
Arif Mustofa <a.mustofa@gmail.com>
2017-05-09 21:58:48 -0400

    Added python assignments - upto list2dict

--- Python/Assignments/MultipleSumAverage.py
+++ Python/Assignments/MultipleSumAverage.py
@@ -1,13 +1,14 @@
 # Multiples - part 1
 def printOdds():
-    for sum in range(0, 1000):
+    for sum in range(0, 1000): # modulo is an good way of solving this problem, is there another way of solving this problem, perhaps saving your computer from having to count and %'ing all 999 numbers?
         if sum % 2 != 0:
             print(sum)
 printOdds()
 
 # Multiples - part 2
 def printAllFive():
-    for five in range(5,1000001):
+    for five in range(5,1000001): # same as above
+    
         if five % 5 == 0:
             print five
 # Uncomment below code to run the program - part 2
