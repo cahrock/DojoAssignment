5d06b109f6a4f0c94ba5f0854711b5986e7bef01
Arif Mustofa <a.mustofa@gmail.com>
2017-05-16 19:32:47 -0400

    MySql

--- Python/flask_fundamentals/ninjaGold/server.py
+++ Python/flask_fundamentals/ninjaGold/server.py
@@ -1,7 +1,7 @@
 from flask import Flask, redirect, render_template, url_for, request, session
 from datetime import datetime
-
 import random
+
 app = Flask(__name__)
 app.secret_key = "hfjdkshf83hjjkj89"
 
@@ -13,41 +13,44 @@ app.secret_key = "hfjdkshf83hjjkj89"
 
 @app.route('/', methods=["POST", "GET"])
 def index():
-    session['total_gold'] = 0
+
+    if 'gold' not in session:
+        session['gold'] = 0
+        session['log'] = [] # set log session
+    else:
+        pass
+
     return render_template("index.html")
 
-@app.route('/process_money', methods=['POST','GET'])
+@app.route('/process_money', methods=['POST'])
 def process():
-
     if request.form['building'] == 'farm':
-        gold = random.randrange(10,20)
-        session["total_gold"] += gold
-        session['time'] = datetime.now()
-        session['building'] = request.form["building"]
-        return render_template('index.html', building=building, time=time,gold=gold)
+        gold = random.randrange(10,21)
+        session['gold'] += gold
+        session['log'].insert(0,("You have earned {} gold - {}".format(gold,datetime.utcnow())))
     elif request.form['building'] == 'cave':
-        gold = random.randrange(5,10)
-        session["total_gold"] += gold
-        time = datetime.now()
-        building = request.form["building"]
-        return render_template('index.html', building=building, time=time,gold=gold)
+        gold = random.randrange(5,11)
+        session['gold'] += gold
+        session['log'].insert(0,("You have earned {} gold - {}".format(gold,datetime.utcnow())))
     elif request.form['building'] == 'house':
-        gold = random.randrange(2,5)
-        session["total_gold"] += gold
-        time = datetime.now()
-        building = request.form["building"]
-        return render_template('index.html', building=building, time=time,gold=gold)
+        gold = random.randrange(2,6)
+        session["gold"] += gold
+        session['log'].insert(0,("You have earned {} gold - {}".format(gold,datetime.utcnow())))
     elif request.form['building'] == 'casino':
-        gold = random.randrange(-50, 50)
-        session["total_gold"] += gold
-        time = datetime.now()
-        building = request.form["building"]
-        return render_template('index.html', building=building, time=time,gold=gold)
+        gold = random.randrange(-50, 51)
+        if gold < 0:
+            session['log'].insert(0,("You have loss - {}".format(datetime.utcnow())))
+        else:
+            session['gold'] = gold
+            session['log'].insert(0,("You have earned {} gold - {}".format(gold,datetime.utcnow())))
 
-@app.route('/clear')
+    return redirect('/')
+
+
+@app.route('/reset')
 def clearsession():
     # Clear the session
     session.clear()
-    return redirect(url_for('index'))
+    return redirect('/')
 
 app.run(debug=True)
